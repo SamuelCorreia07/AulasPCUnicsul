2a94835d9f857455e53299f8a155960dee84bea4
Samuel <thenenouno1@gmail.com>
2025-05-09 20:53:39 -0300

    atividade slide 07 desafio06

---
+++ PCAula08_Repeticao/atividade slide 07/desafio06.py
@@ -0,0 +1,16 @@
+# jogo adivinhe meu num while
+from random import *
+num = randint(0,100)
+i=0
+controle = 0
+
+while controle == 0:
+    i = i+1
+    x = int(input("Digite um número inteiro: "))
+    if num == x:
+        print("Parabéns, você acertou em %d tentativas" %i)
+        controle = 1
+    elif num > x:
+        print("O número pensado é maior!")
+    else:
+        print("O número pensado é menor!")
